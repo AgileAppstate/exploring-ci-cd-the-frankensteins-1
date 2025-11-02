a8266c6c005590d70aea44c48783d509b65a86f2
HopperAlex04 <hopperalex04@gmail.com>
2025-11-02 11:23:10 -0500

    Implemented CountStuff, a helper which counts the characters, including spaces, and lines in a file

--- CountStuff.java
+++ CountStuff.java
@@ -1,3 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CountStuff{
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length > 0) {
+            String fileName = args[0];
+
+            File in = new File(fileName);
+
+            Scanner read = new Scanner(in);
+
+            int characters = 0;
+            int lines = 0;
+
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                characters += line.length();
+                lines += 1;
+            }
+
+            System.out.printf("%s, characters: %d, lines: %d", fileName, characters, lines);
+        }
+    }
 }
