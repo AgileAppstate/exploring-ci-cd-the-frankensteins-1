444d134ee97186e69662e8905b7e90ae1e120481
Adradd <raddantac@appstate.edu>
2025-11-03 13:24:37 -0500

    Creted helloworldtests file

---
+++ helloworldtests.java
@@ -0,0 +1,28 @@
+import java.io.ByteArrayOutputStream;     
+import java.io.PrintStream;
+
+// import static org.junit.test.jupiter.api.Assertions.*;
+
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
+
+ public class helloworldtests {
+//     private ByteArrayOutputStream baos;
+//     private PrintStream oldOut;
+
+//     @BeforeEach
+//     public void beforeEach()
+//     {
+//         this.oldOut = System.out;
+//         this.baos = new ByteArrayOutputStream();
+//         System.setOut(new PrintStream(baos));
+//     }
+
+
+//     @AfterEach
+//     public void afterEach()
+//     {
+//         System.setOut(oldOut);
+//     }
+}
